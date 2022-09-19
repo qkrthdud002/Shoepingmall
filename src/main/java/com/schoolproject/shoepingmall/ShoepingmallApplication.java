12fdaace554165b38f2fb27706428994112c021b
qkrthdud002 <qkrthdud002@gmail.com>
2022-09-19 12:02:20 +0900

    ShoePingMall

---
+++ src/main/java/com/schoolproject/shoepingmall/ShoepingmallApplication.java
@@ -0,0 +1,13 @@
+package com.schoolproject.shoepingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoepingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoepingmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolproject/shoepingmall/ShoepingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolproject.shoepingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoepingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
