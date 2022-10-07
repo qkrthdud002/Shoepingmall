ef90e02a8c71b17f485d47abb8daf7f0d748f556
qkrthdud002 <qkrthdud002@gmail.com>
2022-10-07 11:25:54 +0900

    UserRepository 추가 사항

--- src/main/java/com/schoolproject/shoepingmall/entity/user/repo/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.schoolproject.shoepingmall.entity.user.repo;
-
-import com.schoolproject.shoepingmall.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
