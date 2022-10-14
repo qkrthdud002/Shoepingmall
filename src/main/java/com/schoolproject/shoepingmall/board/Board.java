41454d495d15dbb4cbbb20e9b8e4ce66c9069647
qkrthdud002 <qkrthdud002@gmail.com>
2022-10-14 21:31:20 +0900

    3주차 - BoardEntity 수정

--- src/main/java/com/schoolproject/shoepingmall/board/Board.java
+++ src/main/java/com/schoolproject/shoepingmall/board/Board.java
@@ -1,6 +1,7 @@
 package com.schoolproject.shoepingmall.board;
 
 import com.schoolproject.shoepingmall.user.User;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,11 +17,28 @@ public class Board {
     @Column(name = "board_id")
     private Long id;
 
-    @Column(length = 15, nullable = false)
-    private String name;
+    @Column(length = 15, nullable = false, name = "prize_name")
+    private String prizeName;
+
+    @Column(length = 1000)
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    public Board(String prizeName, String content, User user) {
+        this.prizeName = prizeName;
+        this.content = content;
+        this.user = user;
+        // this(자기 자신을 가리킴.)는 Board
+        user.getBoardList().add(this);
+    }
+
+    public void modify(String prizeName, String content) {
+        this.prizeName = prizeName;
+        this.content = content;
+    }
 
 }
