4db3eb76a53c9f622dbdae28b2b1abaa718633d1
qkrthdud002 <qkrthdud002@gmail.com>
2022-10-14 21:29:57 +0900

    3주차 - UserService 수정

--- src/main/java/com/schoolproject/shoepingmall/user/service/UserService.java
+++ src/main/java/com/schoolproject/shoepingmall/user/service/UserService.java
@@ -6,7 +6,13 @@ import com.schoolproject.shoepingmall.user.dto.UserInsertDTO;
 import com.schoolproject.shoepingmall.user.dto.UserUpdateDTO;
 
 public interface UserService {
+
+    /**
+     * join : 회원가입
+     * 수정 기능 업애기
+     * delete : 로그아웃
+     * */
     public User join(UserInsertDTO userInsertDTO);
-    public User update(UserUpdateDTO userUpdateDTO);
-    public void delete(UserDeleteDTO userDeleteDTO);
+//    public User update(UserUpdateDTO userUpdateDTO);
+//    public void delete(UserDeleteDTO userDeleteDTO);
 }
--- src/main/java/com/schoolproject/shoepingmall/user/service/UserServiceImpl.java
+++ src/main/java/com/schoolproject/shoepingmall/user/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.schoolproject.shoepingmall.user.service;
 
 import com.schoolproject.shoepingmall.exception.DuplicateNameException;
+import com.schoolproject.shoepingmall.exception.WrongIdException;
 import com.schoolproject.shoepingmall.user.User;
 import com.schoolproject.shoepingmall.user.dto.UserDeleteDTO;
 import com.schoolproject.shoepingmall.user.dto.UserInsertDTO;
@@ -10,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -23,7 +26,7 @@ public class UserServiceImpl implements UserService {
     public User join(UserInsertDTO userInsertDTO) {
 
         if(userRepository.existsByUsername(userInsertDTO.getUsername())){
-            throw new DuplicateNameException("이름 중복");
+            throw new DuplicateNameException(userInsertDTO.getUsername());
         }
 
         User user = User.builder()
@@ -35,13 +38,26 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
-    @Override
-    public User update(UserUpdateDTO userUpdateDTO) {
-        return null;
-    }
-
-    @Override
-    public void delete(UserDeleteDTO userDeleteDTO) {
-
-    }
+//    @Override
+//    public User update(UserUpdateDTO userUpdateDTO) {
+//
+//        //db user를 들고와
+//        User user = userRepository.findById(userUpdateDTO.getId()).orElseThrow(() -> new WrongIdException(userUpdateDTO.getId()));
+//
+//        if(userRepository.existsByUsername(userUpdateDTO.getUsername()) &&
+//        user.getUsername().equals(userUpdateDTO.getUsername())){
+//            throw new DuplicateNameException(userUpdateDTO.getUsername());
+//        }
+//        //들고온 user를 수정
+//        user.modify(userUpdateDTO.getUsername(), userUpdateDTO.getPassword());
+//
+//        return user;
+//    }
+//
+//    @Override
+//    public void delete(UserDeleteDTO userDeleteDTO) {
+//        User user = userRepository.findById(userDeleteDTO.getId()).orElseThrow(() -> new WrongIdException(userDeleteDTO.getId()));
+//
+//        userRepository.delete(user);
+//    }
 }
