aeaafe86afdf15e2c1f930a7954c5f7985a1629c
qkrthdud002 <qkrthdud002@gmail.com>
2022-11-01 14:11:03 +0900

    😆 5주차 - DTO 작성

---
+++ src/main/java/com/schoolproject/shoepingmall/buy/dto/BuyDeleteDTO.java
@@ -0,0 +1,23 @@
+package com.schoolproject.shoepingmall.buy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class BuyDeleteDTO {
+
+    @NotNull
+    private Long buyId;
+
+    @NotNull
+    private int count;
+
+    @NotNull
+    private int price;
+
+}
