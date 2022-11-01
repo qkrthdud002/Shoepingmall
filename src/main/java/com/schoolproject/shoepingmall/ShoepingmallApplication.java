21d6b2aaff9bf55a298f16f5511fdd109921441e
qkrthdud002 <qkrthdud002@gmail.com>
2022-11-01 14:10:33 +0900

    😆 5주차

--- src/main/java/com/schoolproject/shoepingmall/ShoepingmallApplication.java
+++ src/main/java/com/schoolproject/shoepingmall/ShoepingmallApplication.java
@@ -2,6 +2,8 @@ package com.schoolproject.shoepingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @SpringBootApplication
 public class ShoepingmallApplication {
@@ -9,5 +11,13 @@ public class ShoepingmallApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoepingmallApplication.class, args);
 	}
+
+//	@Bean
+//	public PageableHandlerMethodArgumentResolverCustomizer customzie() {
+//		return p -> {
+//			p.setOneIndexedParameters(true); // page 1부터 시작하게 한다.
+//			p.setMaxPageSize(10); // page의 사이즈를 10으로 저장
+//		}
+//	}
 
 }
