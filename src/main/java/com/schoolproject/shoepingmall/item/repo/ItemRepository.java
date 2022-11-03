4af285afac2bf2561ca1ed45743e34b404c36d80
qkrthdud002 <qkrthdud002@gmail.com>
2022-11-03 23:32:22 +0900

    🙂 6주차 - Repository 수정

--- src/main/java/com/schoolproject/shoepingmall/item/repo/ItemRepository.java
+++ src/main/java/com/schoolproject/shoepingmall/item/repo/ItemRepository.java
@@ -1,8 +1,13 @@
 package com.schoolproject.shoepingmall.item.repo;
 
+import com.schoolproject.shoepingmall.board.Board;
 import com.schoolproject.shoepingmall.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByBoard(Board board);
 
 }
