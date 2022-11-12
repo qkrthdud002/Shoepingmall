893f2b2c41295f5fb3264badc4933d8564bbff9f
qkrthdud002 <qkrthdud002@gmail.com>
2022-11-12 23:58:40 +0900

    🙂 6주차 - BuyRepository 수정

--- src/main/java/com/schoolproject/shoepingmall/buy/repo/BuyRepository.java
+++ src/main/java/com/schoolproject/shoepingmall/buy/repo/BuyRepository.java
@@ -4,5 +4,4 @@ import com.schoolproject.shoepingmall.buy.Buy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BuyRepository extends JpaRepository<Buy, Long> {
-
 }
